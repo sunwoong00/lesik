590038e70ccaafa795393cf2ab72d6bcad788d61
hoongel1004 <syj000229@naver.com>
2022-11-14 23:45:02 +0900

    revise

--- lesik.py
+++ lesik.py
@@ -934,7 +934,8 @@ def create_sequence(node, coref_dict, ingredient_dict, ingredient_type_list, mix
 
     # 소분류 규격 추가
     sequence_list = add_standard(node, sequence_list)
-    total_sequencelist.append(sequence_list)
+    for sequence in sequence_list:
+        total_sequencelist.append(sequence)
     
     # 화구존/전처리존 분리
     sequence_list = select_cooking_zone(sequence_list)
